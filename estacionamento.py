13694e11184cb35492a4393a6310ed2e9cbdaa26
vitorrossoni <vitorrossonimachado@gmail.com>
2025-11-25 15:53:50 -0300

    trtth

--- estacionamento.py
+++ estacionamento.py
@@ -3,12 +3,12 @@ import random
 
 
 
-print("ola,seja bem vindo ao sistema de estacionamento do shopping\n" \
+print("ola,seja bem vindo ao estacionamento\n" \
 "tabela de preços:1 hora = R$10,00, menos de uma hora = R$00,00")
 
-entrada:datetime
-saida:datetime
-tempo = saida - entrada
+entrada:datetime.time
+saida:datetime.time
+tempo:datetime.time
 tarifa = 10
 pagamento = ["pix","debito","credito","dinheiro"]
 carrinho = []
@@ -19,30 +19,33 @@ encerrar = ""
 
 
 
-if tempo <= 1:
-    valor = tarifa
-else:
-    valor = tempo * tarifa
 
 
-my_datetime = datetime(2025, random.randint(1,12),random.randint(1,28), random.randint(0,24),random.randint(0,59), 0)
-print("Tempo estacionado:", tempo, "hora(s)")
-print("Valor a pagar: R$", round(valor, 2))
+month = random.randint(1,12)
+entrada = datetime.datetime(2025, month,random.randint(1,31), random.randint(0,24),random.randint(0,59), 0)
+saida = datetime.datetime(2025, month,random.randint(entrada.day,31), random.randint(entrada.hour,24),random.randint(0,59), 0)
 
 
 
 
 
+print(f'\ndata de entrada e saida:{entrada} {saida}\n')
 
+def calculateTime():
+     return saida.hour - entrada.hour
+
+valorTotal = calculateTime() * tarifa
+print(f"valor total:{valorTotal}\n")
 
 
 while sair == False:
     print(pagamento)
-    print (f"escolha uma forma de pgaento:\n\pix [0],debito [1],credito [2],dinheiro[3]\n{pagamento[0]}\n{pagamento[1]}\n{pagamento[2]}\n{pagamento[3]}")
+    print (f"escolha uma forma de pagamento:\n\pix [0],debito [1],credito [2],dinheiro[3]\n{pagamento[0]}\n{pagamento[1]}\n{pagamento[2]}\n{pagamento[3]}")
     escolha = int(input())
     if  escolha == 0:
          print("voce escolheu",pagamento[0])
          carrinho.append(pagamento[0])
+         
     elif escolha == 1:
          print("voce escolheu",pagamento[1])
          carrinho.append(pagamento[1])
@@ -53,10 +56,15 @@ while sair == False:
          print("voce escolheu",pagamento[3])
          carrinho.append(pagamento[3])
 
-    encerrar = input()
+    #encerrar = input()
+    break
 
 
 
-print("\n\n\n\n\n\n\n==========recibo==========\n tempo de permanencia", tempo)
-print(" valor total de:", valor)
-print(" forma de pagamento",carrinho )
+print("\n=============RECIBO=============")
+print(f"tempo de permanencia:{calculateTime()} horas")
+print(f"valor total:{valorTotal}")
+print(f"forma de pagamento selecionada:{carrinho}")
+
+
+
